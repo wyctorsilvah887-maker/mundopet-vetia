ab95ac00440013f4eb86bebf1132a06e4a9943cb
Wyctor Sv <wyctorsv7@gmail.com>
2026-03-29 17:59:04 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:1053755044344:web:468b8ec4cc1bf6aee0a60d",
   "apiKey": "AIzaSyAPHZxbPLjhpBFEGdnwfeRVR56z41ryUOQ",
   "authDomain": "studio-6276490711-dfc3e.firebaseapp.com",
+  "storageBucket": "studio-6276490711-dfc3e.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "1053755044344"
 };
