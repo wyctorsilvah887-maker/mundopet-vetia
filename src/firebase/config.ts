2b28b56e6d7319680514b3af7be21959ef803d49
Wyctor Sv <wyctorsv7@gmail.com>
2026-05-02 19:45:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3460818090-be3f1",
+  "appId": "1:1025671364558:web:cf871f6d8917d7cd07b59f",
+  "apiKey": "AIzaSyC9BqzRRIJkE11AF_bVCPBYCowiD7reRvM",
+  "authDomain": "studio-3460818090-be3f1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1025671364558"
+};
